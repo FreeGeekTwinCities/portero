3703ede5dd04cc544446d2cc2b1e4e2074822765
Brian Dunnette <brian@dunnette.us>
2012-11-09 21:25:26 -0600

    adding code comments

--- portero.py
+++ portero.py
@@ -9,18 +9,19 @@ from datetime import date
 config = config.set_trytond(database_name='test', user='admin', password='test')
 print config
 
+#Look up company model, and get name of first company - we'll use this for e.g. welcome page
 Company = Model.get('company.company')
 company = Company.find()[0].party.name
 
+#Get Tryton models for later use
 Work = Model.get('timesheet.work')
-	
 Employee = Model.get('company.employee')
-
 TimesheetLine = Model.get('timesheet.line')
 
 app = Flask(__name__)
 app.debug = True
 
+#Set up timesheet fields for later use
 class TimesheetForm(Form):
 	date = DateField('Date')
 	hours = DecimalField('Hours')
@@ -28,12 +29,14 @@ class TimesheetForm(Form):
 	employee = SelectField('Volunteer')
 	work = SelectField('Work')
 
+#Display welcome page at root of site
 @app.route("/")
 def hello():
 	return render_template('hello.html', company=company)
 
 @app.route("/timesheet", methods=['GET', 'POST'])
 def enter_timesheet():
+	#If there's POST data, construct & save a new timesheet line
 	if request.method == 'POST':
 		line = TimesheetLine()
 		line.hours = float(request.form['hours'])
@@ -43,10 +46,14 @@ def enter_timesheet():
 		#line.date = date(request.form['date'])
 		line.save()
 
+	#Generate timesheet entry form (defined in TimesheetForm above)
 	form = TimesheetForm(request.form)
+	#Get list of available work types, and add them as options for "work" field on form
 	form.work.choices = [(work.id, work.name) for work in Work.find()]
+	#Get list of active volunteers/employees, and add these as options for "employee" field
 	form.employee.choices = [(employee.id, employee.party.name) for employee in Employee.find([('company', '=', 1)])]
 
+	#Finally, render timesheet page
 	return render_template('timesheet.html', form=form, company=company)
 
 @app.route("/transaction")
