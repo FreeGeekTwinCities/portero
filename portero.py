71f56d37c2639e0593ae152fa5c8d5c952d00247
Brian Dunnette <brian@dunnette.us>
2012-11-09 19:45:01 -0600

    timesheet form saves test data to Tryton; still need to have it submit actual form data (esp. Employee and Work)

--- portero.py
+++ portero.py
@@ -1,36 +1,52 @@
 import pkg_resources
 pkg_resources.require("Flask")
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from wtforms import Form, DateField, DecimalField, TextField, SelectField, validators
 from proteus import config, Model
  
-tryton_config = config.set_trytond(database_name='test', user='admin', password='test')
-print tryton_config
+config = config.set_trytond(database_name='test', user='admin', password='test')
+print config
+
+Company = Model.get('company.company')
+company = Company.find()[0].party.name
+
+Work = Model.get('timesheet.work')
+	
+Employee = Model.get('company.employee')
+
+TimesheetLine = Model.get('timesheet.line')
 
 app = Flask(__name__)
+app.debug = True
+
+class TimesheetForm(Form):
+	date = DateField('Date')
+	hours = DecimalField('Hours')
+	description = TextField('Description')
+	employee = SelectField('Volunteer')
+	work = SelectField('Work')
 
 @app.route("/")
 def hello():
-	Party = Model.get('party.party')
-	parties = Party.find()
-	party_array = []
-	for party in parties:
-		party_array.append({'code': party.code, 'name': party.name})
-	print party_array
-	
-	Work = Model.get('timesheet.work')
-	works = Work.find()
-	work_array = []
-	for work in works:
-		work_array.append({'id': work.id, 'name': work.name})
-		
-	Employee = Model.get('company.employee')
-	employees = Employee.find([('company', '=', 1)])
-	employee_array = []
-	for employee in employees:
-		employee_array.append({'id': employee.id})
-	
-	return render_template('hello.html', party_array=party_array, work_array=work_array, employees=employee_array)
+	return render_template('hello.html', company=company)
+
+@app.route("/timesheet", methods=['GET', 'POST'])
+def enter_timesheet():
+	if request.method == 'POST':
+		line = TimesheetLine()
+		print line.__dict__
+		line.save()
+
+	form = TimesheetForm(request.form)
+	form.work.choices = [(work.id, work.name) for work in Work.find()]
+	form.employee.choices = [(employee.id, employee.party.name) for employee in Employee.find([('company', '=', 1)])]
+
+	return render_template('timesheet.html', form=form, company=company)
+
+@app.route("/transaction")
+def enter_transaction():
+	return "stub"
 	
 if __name__ == "__main__":
     app.run()
